35677d29f27f49724624dc127ba10bdd914ccda9
Valeriya2511 <finsternis.ru@mail.ru>
2023-04-12 23:31:47 +0600

    feat: task count cats completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(cat) {
+  let cats = 0;
+  for(let elem of cat) {
+    for (let el of elem) {
+      if (el === '^^'){
+        cats += 1
+      }
+    }
+  }
+  return cats
 }
 
 module.exports = {
