4077b1d02cb3566113425575773a00460092ac92
Valeriya2511 <finsternis.ru@mail.ru>
2023-04-15 22:19:44 +0600

    feat: task completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error ('\'arr\' parameter must be an instance of the Array!')
+  let  lengthArr = arr.length
+  let a = [];
+  let j = 0;
+
+  if (arr.length > 0) {
+    arr.forEach((elem, index) => {
+      if (elem === '--double-next') {
+        if(arr[index+1] === undefined) elem = undefined
+        a.push(arr[index+1])
+      } else if(elem === '--discard-prev'){
+        if(arr[index-1] === undefined) elem = undefined
+        a.pop(arr[index+1])
+      } else if (elem === '--double-prev') {
+        if (arr[index-1] === undefined) elem = undefined;
+        a.push(arr[index-1])
+      } else if (elem === '--discard-next'){
+        if(arr[index+1] === undefined) elem = undefined;
+        arr[index+1] = undefined
+      } else {
+        a.push(elem)
+      }
+    })
+  }
+
+  return a.filter(el => el !== undefined)
 }
 
 module.exports = {
