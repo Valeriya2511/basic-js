d347789f0c1e35d0ced9dd615e2a6ed421c9956d
Valeriya2511 <finsternis.ru@mail.ru>
2023-12-23 18:14:43 +0600

    feat: solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(value = true) {
+    this.value = value;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let scringUpper = string.toUpperCase();
+    let keyUpper = key.toUpperCase();
+    let result = '';
+    let count = 0;
+
+    if (scringUpper.length > keyUpper.length) {
+      keyUpper = keyUpper.repeat(Math.ceil(scringUpper.length / keyUpper.length)).slice(0, scringUpper.length);
+    }
+
+    for (let i = 0; i < scringUpper.length; i++) {
+      if (this.alphabet.includes(scringUpper[i])) {
+        result += this.alphabet[(this.alphabet.indexOf(scringUpper[i]) + this.alphabet.indexOf(keyUpper[i - count])) % this.alphabet.length];
+      } else {
+        result += scringUpper[i];
+        count += 1;
+      }
+    }
+    if (this.value === false) {
+      return result.split('').reverse().join('');
+    }
+    return result;
+  }
+
+  decrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const scringUpper = string.toUpperCase();
+    let keyUpper = key.toUpperCase();
+    let result = '';
+    let count = 0;
+
+    if (scringUpper.length > keyUpper.length) {
+      keyUpper = keyUpper.repeat(Math.ceil(scringUpper.length / keyUpper.length)).slice(0, scringUpper.length);
+    }
+    
+    for (let i = 0; i < scringUpper.length; i += 1) {
+      if (this.alphabet.includes(scringUpper[i])) {
+        result += this.alphabet[(this.alphabet.indexOf(scringUpper[i]) - this.alphabet.indexOf(keyUpper[i - count]) + this.alphabet.length) % this.alphabet.length];
+      } else {
+        result += scringUpper[i];
+        count += 1;
+      }
+    }
+    if (this.value === false) {
+      return result.split('').reverse().join('');
+    }
+    return result;
   }
 }
 
